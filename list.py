408d4775f672097c5d80f821f7d5abc7b5e45cbc
unknown <sagar182283@gmail.com>
2022-11-08 21:24:54 -0800

    new python course file

---
+++ list.py
@@ -0,0 +1,67 @@
+#use any datatype
+#syntax=[ ]  square braket
+# it is mutable formation
+
+#lst=[4,6.5, 'sagar' ,True]
+#print(lst)  
+#print(type(lst))
+
+# 1. sort
+
+lst=[1,3,5,6,6,2,8]
+#lst.sort()
+#print(lst)
+
+# 2|) reverse
+#lst.reverse()
+#print(lst)
+
+# 3)append   (carry single element)
+
+#lst.append(50)
+#print(lst)
+
+#4) extend    (to add multiple element in list)
+
+#lst.extend([10,22])
+#print(lst)
+
+#5) POP  (remove last elemnt from list)
+
+#lst.pop()
+#print(lst)
+
+# 6) remove()
+#lst.remove(5)
+#print(lst)
+
+#7) len(?)  count all element
+#print(len(lst))
+
+#8) sum   (add all alemnt)
+
+#print(sum(lst))
+
+#9 ) index 
+
+#lst[2]=5
+#print(lst)
+
+#10) clear
+#lst.clear()
+#print(lst)
+
+#11) delete 
+#del lst[3]
+#print(lst)
+
+#12) count  ()
+#print(lst.count(6))
+
+#13) insert
+#lst.insert(3,5)
+#print(lst)
+
+#14) copy
+ss=lst.copy()
+print(ss)
