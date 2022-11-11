f3060ae6ae56dfca03439fa5157a3c5d6888e85e
sagar <sagar182283@gmail.com>
2022-11-10 21:03:49 -0800

    sdhdsajk

--- list.py
+++ list.py
@@ -1,3 +1,4 @@
+#  # # LIST  # # #
 #use any datatype
 #syntax=[ ]  square braket
 # it is mutable formation
@@ -7,27 +8,23 @@
 #print(type(lst))
 
 # 1. sort
-
-lst=[1,3,5,6,6,2,8]
+#lst=[1,2,3,4,5,6]
 #lst.sort()
 #print(lst)
 
-# 2|) reverse
+# 2) reverse
 #lst.reverse()
 #print(lst)
 
 # 3)append   (carry single element)
-
 #lst.append(50)
 #print(lst)
 
 #4) extend    (to add multiple element in list)
-
 #lst.extend([10,22])
 #print(lst)
 
 #5) POP  (remove last elemnt from list)
-
 #lst.pop()
 #print(lst)
 
@@ -35,33 +32,31 @@ lst=[1,3,5,6,6,2,8]
 #lst.remove(5)
 #print(lst)
 
-#7) len(?)  count all element
+#7) len()  count all element
 #print(len(lst))
 
 #8) sum   (add all alemnt)
-
 #print(sum(lst))
 
-#9 ) index 
-
+#9 ) index (replace index no from list which is given)
 #lst[2]=5
 #print(lst)
 
-#10) clear
+#10) clear (clear list)
 #lst.clear()
 #print(lst)
 
-#11) delete 
+#11) delete (del input element)
 #del lst[3]
 #print(lst)
 
-#12) count  ()
+#12) count  (count  input element)
 #print(lst.count(6))
 
-#13) insert
+#13) insert (insert at index no)
 #lst.insert(3,5)
 #print(lst)
 
-#14) copy
-ss=lst.copy()
-print(ss)
+#14) copy  (copy element from original list to new list)
+#ss=lst.copy()
+#print(ss)
---
+++ set.py
@@ -0,0 +1,60 @@
+#       SET
+# 1)unoedered
+# 2)syntax={}
+# 3) set is unique ,does not store repeted value
+# 4)set is unchanged but we can remove old iteams and 
+#set={1,2,3,4,5}                       #TYPE
+#print(set)
+#print(type(set))
+
+#set={1,2,2,3,3,4,4,5}                 #TAKE NEW ELEMENT   
+#print(set)
+#print(type(set))
+
+#set={1,3,5,7,9,2,2.5,'sagar',True}
+#print(set)
+
+#set={True, False,'str',1,2,3,6,5,7,5.3}               #UNORDERED
+#print(set)
+
+#set={1,2,3,4}         #  ######## count
+#print(len(set))
+
+#set={1,2,3,4,5,6}
+#set.add((7))
+#print(set)
+
+#set1={1,2,3}             #add
+#set2={4.5,6}
+#set1.update(set2)
+#print(set1)
+
+
+#set={1,2,3,4}     #clear
+#set.clear()
+#print(set)
+
+#set={1,2,3}     #remove
+#set.remove(3)
+#print(set)
+
+#set={1,2,3,4}
+#del set{3}
+#print(set)
+
+
+
+#  assignment    1>nonrepeated  2.add 10   3.remove  (convert tup to set)
+#tup=(1,1,3,5,5,9,8)
+#set=set(tup)
+#print(type(set))
+#set.remove(9)
+#set.add(10)
+#tup=tuple(set)
+#print(tup)
+
+
+
+
+
+
