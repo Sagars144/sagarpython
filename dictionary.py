d9c1edf7584f5df3da05792d9a247ec432c9085f
Sagars144 <117706368+Sagars144@users.noreply.github.com>
2022-11-14 19:23:05 -0800

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,27 @@
+#DICTIONARY
+#syntax={}
+#ordered,changeple
+#remove old element and print new one
+
+
+dict1={1:'sagar',2:'niraj',3:'kunal',3:'hitesh'}
+#print(dict)
+#print(type(dict))
+#print(len(dict))            #count
+       
+#print(dict.get(4))     #acceces
+#dict.keys()@########
+
+#dict[1]='apple'      #for change name 
+#print(dict)
+
+#dict.values()
+#print(dict)
+
+#dict[5]='orange'     #add new element
+#print(dict)
+#dict.pop(3)
+#print(dict)
+
+del dict1
+print(dict1)
