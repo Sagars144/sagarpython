8e6ab988b64d9ea11252b8c945278238ef6f2b44
Sagars144 <117706368+Sagars144@users.noreply.github.com>
2022-11-14 21:36:43 -0800

    Add files via upload

---
+++ new1.py
@@ -0,0 +1,69 @@
+# OPERATORS 
+
+# 1] arithmatic operator
+#a= 50
+#b= 10
+
+#  addition
+#print(a+b)
+
+#  substracation
+#print(a-b)
+
+#  multiplication
+#print(a*b)
+
+# DIVIDE
+#print(a/b)
+
+#  floor division
+#print(a//b)
+
+#  module
+#print(a%b)
+ 
+# 2]assignment operator
+
+#a=5
+#b=15
+#a+=5
+#print(a+b)
+
+#a*=6
+#print(a)
+
+#a**=2
+#print(a)
+
+# 3]comparision
+
+#a=6
+#print(a!=5)
+
+#a=7
+#print(a>15)
+
+# 4]logical operator
+
+#a=20
+#print(a<30 and a>10)
+
+#a=10
+#print(a<50 or a>10)
+
+# 5]IDENTITY OPERATOR
+
+#a=10
+#print(a is 10)
+
+#a=50
+#print(a is not 50)
+
+#  6]MEMBERSHIP OPERATOR
+
+#a=[1,2,3,4,5]
+#print(5 in a )
+
+#a=[20]
+#print(20 not in a)
+
