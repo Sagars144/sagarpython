d3ff32d2ef8784dd613b4081df74aac66c6e9f94
Sagars144 <117706368+Sagars144@users.noreply.github.com>
2022-11-15 20:51:16 -0800

    Add files via upload

---
+++ func.py
@@ -0,0 +1,34 @@
+#def add():
+#    a=20
+#    b=65
+#    print(a+b)
+#add()
+
+#def multi()
+#    a=2
+#    b=10
+ #   
+#    print(a*b)
+#multi() 
+
+#def sum(a,b,c):       #parameter
+#    print(a+b+c)
+#sum(10,20,30)
+
+#def gritting(a):
+#    print(a)
+#gritting('good morning')    
+#gritting("all")
+
+#def add(d,c,b,a):
+#    print(a+b+c+d)
+#add(a=10,b=20,c=30,d=40)
+
+#def mem(c4,c3,c2,c1):
+#    print(c4)
+#mem(c4='sagar',c2='suraj',c3='niraj',c1='hitesh')
+
+#def fam(c1='sagar',c3="niraj",c2="hitesh"):
+#    print(c2)
+#fam()
+
