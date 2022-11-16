4e11bf0e1f7377dd641e23e6d56a174059606d59
Sagars144 <117706368+Sagars144@users.noreply.github.com>
2022-11-15 19:30:09 -0800

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,61 @@
+# TUPLE  
+
+# 1)syntax = ()   round braket
+# 2)value is fix ,  does not change
+
+# 3)tuple is in ordered
+# 4)allow duplicate element
+ 
+#tup=(1,2,3,4,5,6,7,7.5)
+#print(tup)
+#print(type(tup))
+
+# ###########  for single element
+#tup=(1,)
+#print(type(tup))
+#lst=[1,2,3,4]
+#lst[1]=3
+
+#tup=(1,2,3)
+#tup(1)=4
+
+########################for change tuple to list or list to tuple
+
+#tup=(1,2,3,4,5,6,7,7.5)
+
+#lst=list(tup)
+#type(lst)
+#lst[1]=5
+#print(lst)
+
+#tup=tuple(lst)
+#print(tup)
+
+# ######################  duplicate element allow (1,1,2,2,3,3)
+#tup=(1,2,2,3,3,4,4)
+#print(tup)
+
+#print(tup[2])       #for acceces one element
+#print(tup[1:5])      #add one element extra
+
+# ###############################  add two tuple
+
+#tup1=(1,2,3,4,5)
+#tup2=(8,9,10)
+#tup1=tup1+tup2
+#print(tup1)
+
+#print(len(tup1))   #### count
+   
+# ### add 6,  want [3]=10
+
+#tup=(1,2,3,4)
+#lst=list(tup)
+#lst[3]=10
+#print(lst)
+            # add element
+#lst.append(6)
+#print(lst)
+#lst.insert(4,10)
+#tup=tuple(lst)
+#print(tup)
