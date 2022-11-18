fd07e1d9d330a6d03a45ca0b9ad5a1ca652db3e7
Sagars144 <117706368+Sagars144@users.noreply.github.com>
2022-11-17 21:17:44 -0800

    Add files via upload

---
+++ if.py
@@ -0,0 +1,72 @@
+# #if-elif-else
+# #
+
+# x=1
+# if x==2:
+#     print('yes its 2')
+# if x==1:
+#     print('yes its 1')
+# if x==3:
+#     print('yes its 3')
+# if x==4:
+#     print('yes ists 4')     
+
+# a=int(input("entre any number"))
+# if a>50:
+#     print("true")
+# else :
+#     print("false")
+
+# child=int(input("entre age :"))
+# if child<10:
+#     print("baby")
+# elif child<60:
+#     print('young')
+# elif child>100:
+#     print("wrong input")
+# else :
+#     print('old')    
+
+# num=int(input('entre any number:'))
+# if (num%2==0):
+#     print('even')
+# else:
+#     print('odd')    
+
+#NESTED IF
+
+# a=int(input('entre num'))
+# if a<20:
+#     if a%2==0:
+#         print('even')
+# else:
+#     print("no")        
+    
+# a=input('please entre name')
+# if a=='sagar':
+#     print('plese call on 8422073234')
+# else:
+#     print('go')
+
+# a=input('entre name:')
+# b=int(input('entre number:'))
+# if a=='sagar' or 'sai':
+
+#     if b==10:
+#         print('call on 2234')
+#     elif b==20:
+#         print('call on 3546') 
+# else:
+#     print('wrong')           
+
+a=input('entre name:')
+b=int(input('entre mail'))
+c=input('sis name')
+if a=='sagar':
+    print('entre name')
+elif b=='1234':
+    print('print sis ')
+if c=='lalita':
+    print('call')
+else:
+    print('go')    
